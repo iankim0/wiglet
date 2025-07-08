5810b7f9f065f4dc2b7a5fb70b35d2e69206ea00
iankim0 <iank0426@gmail.com>
2025-07-08 16:26:07 -0400

    asdf

--- balldragon/Assets/Wiglet.cs
+++ balldragon/Assets/Wiglet.cs
@@ -22,77 +22,66 @@ public class Wiglet : MonoBehaviour {
   }
  }
 
+ float MagicZeroCenteredDeadBand(float x, float deadBandRadius) {
+  float Abs_x = Mathf.Abs(x);
+  int Sign_x = Math.Sign(x);
+  float result = 0.0f;
+  if (Abs_x > deadBandRadius) {
+   result = Sign_x * (Abs_x - deadBandRadius);
+  }
+  return(result);
+ }
 
-
- private GameObject ghostRobot;
- private GameObject robot;
 
 
  private OVRCameraRig OVR_cameraRig;
  private OVRManager OVR_manager;
  private OVRPassthroughLayer OVR_passthroughLayer;
- public OVRHand leftHand;
- public OVRHand rightHand;
- private GestureTracker leftGesture;
- private GestureTracker rightGesture;
 
-
-
- void togglePassThrough() {
-  OVR_passthroughLayer.enabled = !OVR_passthroughLayer.enabled;
-  // OVR_manager.isInsightPassthroughEnabled = passthrough;
- } 
- 
- void setPassthrough(bool passthrough) {
-
- }
- 
-
- bool initialized;  
- private void Awake() {
-  initialized = false;
-
+ void OVR_Init() {
   OVRInput.EnableSimultaneousHandsAndControllers();
 
   OVR_cameraRig = FindObjectOfType<OVRCameraRig>();
   OVR_manager = OVR_cameraRig.GetComponent<OVRManager>();
   OVR_passthroughLayer = OVR_cameraRig.GetComponent<OVRPassthroughLayer>();
+ }
 
-  leftGesture = leftHand.GetComponent<GestureTracker>();
-  rightGesture = rightHand.GetComponent<GestureTracker>();
+ void togglePassThrough() {
+  OVR_passthroughLayer.enabled = !OVR_passthroughLayer.enabled;
+ } 
 
-  ghostRobot = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-  ghostRobot.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-  ghostRobot.GetComponent<Collider>().enabled = false;
-  ghostRobot.GetComponent<Renderer>().material = selectionGhost;
-  ghostRobot.SetActive(false);
+ private GameObject robot;
 
-  robot = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+ bool initialized;  
+ private void Awake() {
+  initialized = false;
+
+  OVR_Init();
+
+  robot = GameObject.CreatePrimitive(PrimitiveType.Cube);
   robot.name = "Robot";
-  robot.transform.localScale = ghostRobot.transform.localScale;
-  robot.transform.position = new Vector3(0f, 0f, 0f);
-  robot.SetActive(false);
+  robot.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
  }
 
  [SerializeField] int phase;
  public void Update() {
 
-  bool A_Pressed               = (Input.GetKeyDown(KeyCode.A)) || (!rightGesture.pinchDown && OVRInput.GetDown(OVRInput.RawButton.A));
+  bool A_Pressed               = (Input.GetKeyDown(KeyCode.A)) || OVRInput.GetDown(OVRInput.RawButton.A);
   bool B_Pressed               = (Input.GetKeyDown(KeyCode.B)) || OVRInput.GetDown(OVRInput.RawButton.B);
-  bool X_Pressed               = (Input.GetKeyDown(KeyCode.X)) || (!leftGesture.pinchDown && OVRInput.GetDown(OVRInput.RawButton.X));
+  bool X_Pressed               = (Input.GetKeyDown(KeyCode.X)) || OVRInput.GetDown(OVRInput.RawButton.X);
   bool Y_Pressed               = (Input.GetKeyDown(KeyCode.Y)) || OVRInput.GetDown(OVRInput.RawButton.Y);
   bool LeftThumbstick_Pressed  = OVRInput.GetDown(OVRInput.Button.PrimaryThumbstick);
   bool RightThumbstick_Pressed = OVRInput.GetDown(OVRInput.Button.SecondaryThumbstick);
   Ray LeftRay; {
    Vector3 rayOrigin;
    Vector3 rayDirection;
-   if ((OVRInput.activeControllerType & OVRInput.Controller.LTouch) == OVRInput.Controller.LTouch) {
+   // if ((OVRInput.activeControllerType & OVRInput.Controller.LTouch) == OVRInput.Controller.LTouch) {
     rayOrigin = OVR_cameraRig.leftControllerInHandAnchor.position;
     rayDirection = OVR_cameraRig.leftControllerInHandAnchor.forward;
-   } else {
-    rayOrigin = leftGesture.indexTip;
-    rayDirection = leftHand.PointerPose.forward;
-   }
+   // } else {
+   // rayOrigin = leftGesture.indexTip;
+   // rayDirection = leftHand.PointerPose.forward;
+   // }
    LeftRay = new Ray(rayOrigin, rayDirection);
   }
   Vector2 LeftThumb = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
@@ -105,53 +94,30 @@ public class Wiglet : MonoBehaviour {
   if (reset) { // reset
    initialized = true;
    phase = 0;
-   ghostRobot.SetActive(true);
-   robot.SetActive(false);
   }
 
   int _phase = 0;
-  Func<bool> PHASE = () => {
-   return(phase == _phase++);
-  };
-  Func<bool> NEXT = () => {
-   bool result = X_Pressed;
-   if (result) ++phase; // NOTE: phase captured by reference
-   return(result);
-  };
-
+  Func<bool> PHASE = () => { return(phase == _phase++); };
+  Func<bool> NEXT = () => { bool result = X_Pressed; if (result) ++phase; return(result); };// NOTE: phase captured by reference
   if (false) {
   } else if (PHASE()) { // prep
-   ghostRobot.transform.position = (LeftRay.origin + (0.08f * LeftRay.direction));
-
+   robot.transform.position = (LeftRay.origin + (0.08f * LeftRay.direction));
    if (NEXT()) {
-
+    ;
    }
   } else if (PHASE()) { // hot
-
-   float ytrans = Mathf.Abs(LeftThumb.y) > 0.6f ? LeftThumb.y - 0.6f : 0.0f;
-   ytrans *= 0.001f;
-   float rotate = Mathf.Abs(LeftThumb.x) > 0.4f ? LeftThumb.x - 0.4f : 0.0f;
-   rotate *= 1.5f;
-   ghostRobot.transform.Translate(new Vector3(0, ytrans, 0));
-   ghostRobot.transform.Rotate(0, rotate, 0);
-
-   if (NEXT()) {
-    robot.SetActive(true);
-    robot.transform.position = ghostRobot.transform.position;
-    robot.transform.rotation = ghostRobot.transform.rotation;
-    ghostRobot.SetActive(false);
+   // FORNOW
+   robot.transform.Translate(new Vector3(0, (0.001f * MagicZeroCenteredDeadBand(LeftThumb.y, 0.6f)), 0));
+   robot.transform.Rotate(0, (1.5f * MagicZeroCenteredDeadBand(LeftThumb.x, 0.4f)), 0);
+  
+  if (NEXT()) {
+    ;
    }
-
   } else if (PHASE()) { // live
-
+   robot.transform.Rotate(2.0f, 1.0f, 0);
    if (NEXT()) {
     initialized = false;
    }
-
   }
-
-  //
-
-
  }
 }
